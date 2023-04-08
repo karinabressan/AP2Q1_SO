e97ef9a7c976018fa9cf8813c48c4e074a887b2a
Karina <karinabressan@outlook.com>
2023-04-08 18:57:37 -0300

    criando arquivo vazio

--- Teste.sh
+++ Teste.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-usuarioArgumento=$1
-# $USER obtem o usuario atual
-usuarioAtual=$USER
 
-# Se o usuário do argumento existe, será utilizado.
-if id -u "$usuarioArgumento" > /dev/null 2>&1; then
-        usuario=$usuarioArgumento
-# Se o usuário do argumento não existir ou não for informado, será utilizado o usuário atual.
-else
-        usuario=$usuarioAtual
-        
-fi
-
-
+>ArquivoTeste.csv
 
 
 # sessao de teste
-echo "usuario escolhido: $usuario"
