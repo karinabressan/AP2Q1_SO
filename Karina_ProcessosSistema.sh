7e57062904131f791f205648d9728e230f02a8a4
Karina <karinabressan@outlook.com>
2023-04-08 19:43:58 -0300

    criando o arquivo csv

--- Karina_ProcessosSistema.sh
+++ Karina_ProcessosSistema.sh
@@ -13,5 +13,14 @@ else
         
 fi
 
+# Variável para guardar a informação da data atual
+hoje=$(date +"%Y%m%d")
+# Variável para criar o nome do arquivo 
+nomeArquivo=$hoje"_Processos_"$usuario".csv"
+# Cabeçalho
+cabecalho="Número total de processos ativos no sistema,Número de processos ativos do usuário,Quantidade total de threads dos processos do usuário,Processo mais antigo do usuário,Nome do usuário corrente,Quantidade de processos sendo executados, excluindo-se os do usuário corrente,Quantidade de processos sendo executados como root"
+# Criando o arquivo com padrão de nome <AAAAMMDD>_Processos_<usuário>.csv
+echo "$cabecalho">$nomeArquivo
+
 # Sessão de teste
 echo "usuario escolhido: $usuario"
--- Teste.sh
+++ Teste.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+usuario=$USER
 
->ArquivoTeste.csv
+# Variável para guardar a informação da data atual
+hoje=$(date +"%Y%m%d")
+# Variável para criar o nome do arquivo 
+nomeArquivo=$hoje"_Processos_"$usuario".csv"
 
+# Cabeçalho
+cabecalho="Número total de processos ativos no sistema,Número de processos ativos do usuário,Quantidade total de threads dos processos do usuário,Processo mais antigo do usuário,Nome do usuário corrente,Quantidade de processos sendo executados, excluindo-se os do usuário corrente,Quantidade de processos sendo executados como root"
+# Criando o arquivo com padrão de nome <AAAAMMDD>_Processos_<usuário>.csv
+echo "$cabecalho">$nomeArquivo
 
-# sessao de teste
+# sessao de teste  
+
+echo "$hoje"
+echo "$nomeArquivo"
